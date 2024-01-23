047051bb46ebc3dc8906a8d31eb86ba557e95bbd
Yusuke Toyama <131319857+koko1928@users.noreply.github.com>
2024-01-23 11:12:09 +0900

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import os
 import threading
 import random
 import string
